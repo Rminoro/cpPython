1f7a09feade43762d87446f45b7bd547c72f8661
rminoro <rminoro@outlook.com>
2023-09-22 20:15:22 -0300

    adicionando alterações no codigo

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
-from flask import Flask
-from flask import request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 app.config["Debug"] = True
 
-users_dict =[]
+# users_dict = [{"id":"1", "Nome":"jose"},{"id":"2","Nome":"Rafael"}]
+# users_dict = [{"id": "1", "Nome": "jose"}, {"id": "2", "Nome": "Rafael"}]
+users_dict = [{"id": "1", "nome": "jose"}, {"id": "2", "nome": "Rafael"}]
 
-@app.route('/users', methods =['GET'])
+@app.route('/users', methods=['GET'])
 def get_users_id():
     users = []
     for user in users_dict:
@@ -14,7 +15,7 @@ def get_users_id():
         for arg in request.args:
             val = user[arg]
             param = request.args[arg]
-            if isinstance(val,int):
+            if isinstance(val, int):
                 param = int(param)
             if isinstance(val, str):
                 val = val.upper()
@@ -23,45 +24,50 @@ def get_users_id():
                 contProp += 1
         if contProp == len(request.args):
             users.append(user)
-    return jsonify(users)
-    # if 'id' in request.args:
-    #     id = int(request.args['id'])
-    # else:
-    #     return "Erro: Id Não identificado, por favor inserir id válido"
     
-    # for user in users_dict:
-    #     if user['id'] == id:
-    #         return jsonify(user)
-    # return{}
+    users_with_names = [{'id': user['id'], 'nome': user['nome']} for user in users]
+    return jsonify(users_with_names)
 
-@app.route('/users/<id>', methods =['GET'])
+@app.route('/users/<id>', methods=['GET'])
 def get_user_id_in_path(id):
     for user in users_dict:
-        if user ['id'] == int(id):
+        if user['id'] == id:
+            
             return jsonify(user)
-    return{}
+    return '', 404
 
 @app.route('/user', methods=['POST'])
 def post_users():
     user = request.get_json()
-    user['id'] = len(users_dict) + 1
+    user['id'] = str(len(users_dict) + 1)
     users_dict.append(user)
-    return jsonify(user)
+    return jsonify(user), 201
 
-@app.route('/user', methods = ['PUT'])
+# @app.route('/user', methods=['PUT'])
+# def put_users():
+#     user = request.get_json()
+#     for i, u in enumerate(users_dict):
+#         if u['id'] == user['id']:
+#             users_dict[i] = user
+#             return '', 204
+#     return '', 404
+@app.route('/user', methods=['PUT'])
 def put_users():
-    user = request.get_json()
-    for i, u in enumerate(users_dict):
-        if u['id'] == user['id']:
-            users_dict[i] = user
-    return{}
+    user_data = request.get_json()  
+    for user in users_dict:
+        if user['id'] == user_data['id']:
+            user['nome'] = user_data['nome']  
+            return '', 204  
+    return '', 404  
+
 
 @app.route('/user/<id>', methods=['DELETE'])
 def delete_users(id):
     for user in users_dict:
-        if user ['id'] == int(id):
+        if user['id'] == id:
             users_dict.remove(user)
-    return{}
+            return '', 204
+    return '', 404
 
 if __name__ == '__main__':
     app.run()
